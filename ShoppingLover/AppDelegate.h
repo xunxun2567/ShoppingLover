a5807c5a71978abec83fa138b0da30e7fa9f752e
konglingkai <konglingkai@egibbon.com>
2012-07-16 09:29:13 +0800

    Initial Commit

---
+++ ShoppingLover/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ShoppingLover
+//
+//  Created by Lingkai Kong on 12-7-16.
+//  Copyright (c) 2012年 Egibbon Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ShoppingLover/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShoppingLover
+//
+//  Created by Lingkai Kong on 12-7-16.
+//  Copyright (c) 2012年 Egibbon Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
